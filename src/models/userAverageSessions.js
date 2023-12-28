f489ef9cfb6c12cc1a2f01820f59fee4001b4910
axido545 <axido545@gmail.com>
2023-12-28 06:55:23 +0100

    recup données sessions averrage  ok

--- src/models/userAverageSessions.js
+++ src/models/userAverageSessions.js
@@ -1,29 +1,38 @@
 export class UserAverageSessions {
     id;
     sessions;
+    day;
+    sessionLength;
+
 
     constructor(getUserAverageSessions) {
-        if (getUserAverageSessions && getUserAverageSessions.sessions) {
-            this.id = getUserAverageSessions.userId;
-            this.sessions = this.processSessions(getUserAverageSessions.sessions)
+        if (getUserAverageSessions && getUserAverageSessions.data) {
+            this.id = getUserAverageSessions.data.userId || null;
+            this.sessions = getUserAverageSessions.data.sessions || [];
+            this.day = getUserAverageSessions.data.day || '';
+            this.sessionLength = getUserAverageSessions.data.sessionLength || '';
+
+            // this.processSessions(getUserAverageSessions.sessions)
         } else {
             this.id = null;
             this.sessions = [];
+            this.day = '';
+            this.sessionLength = '';
         }
     }
-    processSessions(sessionsData) {
-        console.log('Processing sessionsData:', sessionsData);
+    // processSessions(sessionsData) {
+    //     console.log('Processing sessionsData:', sessionsData);
 
-        if (!sessionsData || !Array.isArray(sessionsData)) {
-            console.log('Empty or non-array sessionsData');
-            return [];
-        }
-        const processedSessions = sessionsData.map(session => ({
-            day: session.day || null,
-            sessionLength: session.sessionLength || 0,
-        }));
-        console.log('Processed sessions:', processedSessions);
-        return processedSessions;
-    }
+    //     if (!sessionsData || !Array.isArray(sessionsData)) {
+    //         console.log('Empty or non-array sessionsData');
+    //         return [];
+    //     }
+    //     const processedSessions = sessionsData.map(session => ({
+    //         day: session.day || null,
+    //         sessionLength: session.sessionLength || 0,
+    //     }));
+    //     console.log('Processed sessions:', processedSessions);
+    //     return processedSessions;
+    // }
 
 }
