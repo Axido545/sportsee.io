da395d371b4eb4d1dfa4b628479c321c6e26c538
axido545 <axido545@gmail.com>
2023-12-28 22:34:22 +0100

    performance ok

--- src/charts/performancechart/PerformanceChart.jsx
+++ src/charts/performancechart/PerformanceChart.jsx
@@ -3,33 +3,10 @@ import "./performancechart.css"
 
 export default function performanceChart({ data }) {
     console.log(data)
-
-
     return <div className="performance-chart">
-        {/* <RadarChart outerRadius={90} data={data} width={180} height={180} cx="50%" cy="50%">
-            <PolarGrid radialLines={false} gridType="polygon" />
-            <PolarAngleAxis
-                dataKey="subject"
-                dy={3}
-                tickLine={false}
-                tick={{
-                    fontSize: 10,
-                    fontWeight: 500,
-                    fill: "white",
-                }}
-            />
-            <PolarRadiusAxis tick={false} axisLine={false} />
-            <Radar
-                dataKey="fullMark"
-                fill="red"
-                fillOpacity={0.6}
-                legendType="none"
-            /> */}
-        {/* </RadarChart> */}
         <RadarChart cx="50%" cy="50%" outerRadius="80%" width={190} position="absolute" top={10} right={10} height={180} data={data}>
             <PolarGrid />
             <PolarAngleAxis dataKey="subject" tickLine={false} dy={3}
-
                 tick={{
                     fontSize: 10,
                     fontWeight: 500,
