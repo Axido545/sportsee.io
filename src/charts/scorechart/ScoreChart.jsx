78dedc343a140d5fe59056775e80cc38378615fb
axido545 <axido545@gmail.com>
2023-12-28 13:14:01 +0100

    maj score et session

--- src/charts/scorechart/ScoreChart.jsx
+++ src/charts/scorechart/ScoreChart.jsx
@@ -1,5 +1,5 @@
 import { RadialBarChart, RadialBar, Legend, PolarAngleAxis, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
-
+import "./scorechart.css"
 
 
 export default function ScoreChart({ score }) {
@@ -9,13 +9,13 @@ export default function ScoreChart({ score }) {
     return <div>Score non disponible</div>;
   }
   return <div className="score-chart"
-    style={{ zIndex: "10", color: 'black', position: "absolute", top: '500px', backgroundColor: '#FBFBFB' }}
-  >
+  // style={{ zIndex: "10", color: 'black', position: "absolute", top: '500px', backgroundColor: '#FBFBFB' }}
+  ><p>Score</p>
     <RadialBarChart width={200} height={200} cx={100} cy={120} innerRadius={60} outerRadius={80} barSize={10} data={[score]} startAngle={60} endAngle={216}>
-      <RadialBar dataKey="value" fill="#8884d8" />
+      <RadialBar dataKey="value" fill="#FF0000" radius={10} />
       <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" align="right" />
     </RadialBarChart>
-    <p className='score-title'>Score: {score.value}%</p>
+    <p className='score-title'>{score.value}%<br /> de votre objectif</p>
   </div>
 }
 
--- src/charts/sessionschart/SessionsAverageChart.jsx
+++ src/charts/sessionschart/SessionsAverageChart.jsx
@@ -6,7 +6,7 @@ export default function SessionsAverageChart({ sessions }) {
 
   return (
     <div className='sessionaverage-wrap'>
-      <LineChart width={258} height={250} data={sessions} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <LineChart width={200} height={200} data={sessions} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <XAxis dataKey="day" />
         <YAxis />
         <Tooltip />
